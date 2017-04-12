76d8d01ab04a5b70c97174360f83252a855f645f
markchernov <mark.chernov@gmail.com>
2017-04-11 21:48:13 -0400

    added post routes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=abcdefghijklmnop';
 
@@ -11,4 +14,20 @@ export function fetchPosts() {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(props) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+    return {
+        type: CREATE_POST,
+        payload: request
+    }
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+    return {
+        type: FETCH_POST,
+        payload: request
+    }
 }
